b2300fe3d00aef1e1ee54e06b3bfe2d55332057e
repomaija <maija.reponen@gmail.com>
2026-06-11 13:54:20 +0300

    Add 3.19: add validation for name length, new frontend build

--- index.js
+++ index.js
@@ -66,8 +66,6 @@ app.post('/api/persons', (request, response, next) => {
         .then(existingPerson => {
 
             if (existingPerson) {
-
-                
                 existingPerson.number = body.number
 
                 return existingPerson.save()
@@ -125,13 +123,15 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+    console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
-  next(error)
+    next(error)
 }
 
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -14,7 +14,10 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minlength: 3
+  },
   number: String,
 })
 
