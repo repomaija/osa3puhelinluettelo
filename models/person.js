551ff582e149f71742e297a4606d751e09ae77ef
repomaija <maija.reponen@gmail.com>
2026-06-12 10:09:35 +0300

    Add 3.20 validation for number

--- models/person.js
+++ models/person.js
@@ -18,7 +18,16 @@ const personSchema = new mongoose.Schema({
     type: String,
     minlength: 3
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
